5d44cbfeacae87304437cb0d343929f7813cb91a
Anne Cocos <anne@askiggy.com>
2021-11-11 11:55:32 -0500

    work with quadkeys

--- iggyenrich/iggy_data_package.py
+++ iggyenrich/iggy_data_package.py
@@ -5,13 +5,15 @@ import pandas as pd
 from enum import Enum
 from pathlib import Path
 from pydantic import BaseModel, validator
-from typing import Dict, List, Optional
+from pyquadkey2 import quadkey
+from shapely.geometry import Point, Polygon
+from typing import Dict, List, Optional, Union
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 KNOWN_BOUNDARIES = [
-    "isochrone_walk_10m",
+    "qk_isochrone_walk_10m",
     "cbg",
     "census_tract",
     "county",
@@ -20,6 +22,8 @@ KNOWN_BOUNDARIES = [
     "zipcode",
 ]
 
+# TODO: do not re-load data if loaded already (enable refresh of load boundaries+features)
+
 
 class GeomTypeEnum(str, Enum):
     json = "json"
@@ -27,38 +31,12 @@ class GeomTypeEnum(str, Enum):
 
 
 class IggyDataPackage(BaseModel, abc.ABC):
-    base_loc: str
     iggy_version_id: str
-    parcels_prefix: str
+    crosswalk_prefix: str
     iggy_prefix: str = "unified"
-    geom_type: GeomTypeEnum = GeomTypeEnum.wkt
-    data_loc: Optional[str] = None
-    parcel_loc: Optional[str] = None
-    parcel_data: Optional[gpd.GeoDataFrame] = None
-    boundary_data: Optional[Dict[str, pd.DataFrame]] = {}
 
     class Config:
         arbitrary_types_allowed = True
-
-    @validator("data_loc", pre=True, always=True)
-    def set_data_loc(cls, v, values):
-        geom_spec = ""
-        if values["geom_type"] == GeomTypeEnum.wkt:
-            geom_spec = "-wkt"
-        data_loc_suffix = ""
-        if values["iggy_prefix"] != "unified":
-            data_loc_suffix = f"_{values['iggy_prefix']}"
-        iggy_dir = (
-            f"iggy-package{geom_spec}-{values['iggy_version_id']}{data_loc_suffix}"
-        )
-        return v or str(Path(values["base_loc"]) / iggy_dir)
-
-    @validator("parcel_loc", always=True)
-    def set_parcel_loc(cls, v, values):
-        return v or str(
-            Path(values["data_loc"])
-            / f"{values['parcels_prefix']}_{values['iggy_version_id']}"
-        )
 
     @abc.abstractmethod
     def load(
@@ -75,100 +53,175 @@ class IggyDataPackage(BaseModel, abc.ABC):
         pass
 
 
+def infer_bounds(boundaries: List[str] = [], features: List[str] = []) -> Dict:
+    # infer boundaries to load
+    bounds_features_to_load = {}
+    if boundaries:
+        bounds_features_to_load = {b: [] for b in boundaries if b in KNOWN_BOUNDARIES}
+    if features:
+        for kb in KNOWN_BOUNDARIES:
+            kb_features = [f for f in features if f.endswith(kb)]
+            if kb_features:
+                bounds_features_to_load[kb] = kb_features
+    if not bounds_features_to_load:
+        bounds_features_to_load = {kb: [] for kb in KNOWN_BOUNDARIES}
+    return bounds_features_to_load
+
+
+def qk2tilegeom(qk_str: str) -> Polygon:
+    """Convert quadkey (e.g. `0320212302300330123`) to its corresponding tile Polygon."""
+    qk = quadkey.from_str(qk_str)
+    corners = [
+        quadkey.TileAnchor.ANCHOR_NW,
+        quadkey.TileAnchor.ANCHOR_NE,
+        quadkey.TileAnchor.ANCHOR_SE,
+        quadkey.TileAnchor.ANCHOR_SW,
+        quadkey.TileAnchor.ANCHOR_NW,
+    ]
+    points_xy = [qk.to_geo(c) for c in corners]
+    poly = Polygon([Point(p[1], p[0]) for p in points_xy])
+    return poly
+
+
 class LocalIggyDataPackage(IggyDataPackage):
-    base_loc: str
     iggy_version_id: str
-    parcels_prefix: str
+    crosswalk_prefix: str
+    base_loc: str
     iggy_prefix: str = "unified"
     geom_type: GeomTypeEnum = GeomTypeEnum.wkt
     data_loc: Optional[str] = None
-    parcel_loc: Optional[str] = None
-    parcel_data: Optional[gpd.GeoDataFrame] = None
+    crosswalk_loc: Optional[str] = None
+    crosswalk_data: Optional[gpd.GeoDataFrame] = None
     boundary_data: Optional[Dict[str, pd.DataFrame]] = {}
+    bounds_features: Optional[Dict[str, str]] = {}
+
+    @validator("data_loc", pre=True, always=True)
+    def set_data_loc(cls, v, values):
+        geom_spec = ""
+        if values["geom_type"] == GeomTypeEnum.wkt:
+            geom_spec = "-wkt"
+        data_loc_suffix = ""
+        if values["iggy_prefix"] != "unified":
+            data_loc_suffix = f"_{values['iggy_prefix']}"
+        iggy_dir = (
+            f"iggy-package{geom_spec}-{values['iggy_version_id']}{data_loc_suffix}"
+        )
+        return v or str(Path(values["base_loc"]) / iggy_dir)
+
+    @validator("crosswalk_loc", always=True)
+    def set_geoms_loc(cls, v, values):
+        return v or str(
+            Path(values["data_loc"])
+            / f"{values['crosswalk_prefix']}_{values['iggy_version_id']}"
+        )
 
     def load(self, boundaries: List[str] = [], features: List[str] = []) -> None:
-        # load parcels
-        self.parcel_data = pd.read_parquet(self.parcel_loc)
-        self.parcel_data.parcel_geometry = gpd.GeoSeries.from_wkt(
-            self.parcel_data.parcel_geometry
-        )
-        self.parcel_data = gpd.GeoDataFrame(
-            self.parcel_data, geometry="parcel_geometry", crs="WGS84"
-        )
-        print(f"Loaded {self.parcel_data.shape[0]} parcels from {self.parcel_loc}.")
-        print("Creating spatial index on parcels (this could take a few minutes)...")
-        __ = self.parcel_data.sindex
-        print("Spatial index complete.")
+        # load iggy geoms
+        if self.crosswalk_data is None:
+            self.crosswalk_data = pd.read_parquet(self.crosswalk_loc)
+            self.crosswalk_data.set_index("id", inplace=True)
+            print(
+                f"Loaded {self.crosswalk_data.shape[0]} geometries from {self.crosswalk_loc}."
+            )
+        else:
+            print(f"Crosswalk data already loaded...skipping")
 
         # infer boundaries to load
-        bounds_features_to_load = {}
-        if boundaries:
-            bounds_features_to_load = {
-                b: [] for b in boundaries if b in KNOWN_BOUNDARIES
-            }
-        if features:
-            for kb in KNOWN_BOUNDARIES:
-                kb_features = [f for f in features if f.endswith(kb)]
-                if kb_features:
-                    bounds_features_to_load[kb] = kb_features
-        if not bounds_features_to_load:
-            bounds_features_to_load = {kb: [] for kb in KNOWN_BOUNDARIES}
+        bounds_features_to_load = infer_bounds(boundaries, features)
         print(f"Will load boundaries {bounds_features_to_load.keys()}...")
 
         # load boundaries
         for boundary, boundary_features in bounds_features_to_load.items():
-            bnd_file = (
-                Path(self.data_loc)
-                / f"{self.iggy_prefix}_{boundary}_{self.iggy_version_id}"
-            )
-            df = pd.read_parquet(bnd_file)
-            df.columns = df.columns.map(lambda x: str(x) + f"_{boundary}")
-            if boundary_features:
-                df = df[[f"id_{boundary}"] + boundary_features]
-            self.boundary_data[boundary] = df
-            print(
-                f"Loaded boundary {boundary} with {df.shape[0]} rows and {df.shape[1]} columns"
-            )
+            if boundary_features != self.bounds_features.get(boundary):
+                bnd_file = (
+                    Path(self.data_loc)
+                    / f"{self.iggy_prefix}_{boundary}_{self.iggy_version_id}"
+                )
+                df = pd.read_parquet(bnd_file)
+                df.columns = df.columns.map(lambda x: str(x) + f"_{boundary}")
+                if boundary_features:
+                    keepfeatures = boundary_features
+                    if f"id_{boundary}" not in keepfeatures:
+                        keepfeatures = [f"id_{boundary}"] + keepfeatures
+                    df = df[keepfeatures]
+                self.boundary_data[boundary] = df
+                print(
+                    f"Loaded boundary {boundary} with {df.shape[0]} rows and {df.shape[1]} columns"
+                )
+            else:
+                print(
+                    f"Boundary {boundary} with {self.boundary_data[boundary].shape[0]} "
+                    f"rows and {self.boundary_data[boundary].shape[1]} columns already loaded."
+                )
+        # check for removed boundaries
+        remove_boundaries = []
+        for boundary in self.bounds_features:
+            if boundary not in bounds_features_to_load:
+                remove_boundaries.append(boundary)
+        for rb in remove_boundaries:
+            print(f"Removed data for boundary {rb}")
+            del self.boundary_data[rb]
 
-    def enrich(self, points: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
-        # join input points to parcels
-        points_parcels = points.sjoin(
-            self.parcel_data, how="left", predicate="intersects"
-        )
+        self.bounds_features = bounds_features_to_load
+
+    def enrich(
+        self,
+        points: Union[pd.DataFrame, gpd.GeoDataFrame],
+        latitude_col: str = "latitude",
+        longitude_col: str = "longitude",
+        zoom: int = 19,
+        drop_qk_col: bool = True,
+    ) -> Union[pd.DataFrame, gpd.GeoDataFrame]:
+        # join input points to iggy quadkeys
+        points_ = points.copy()
         if not points.index.name:
-            points.index.name = "points_index"
-        points_parcels = points_parcels.reset_index().drop_duplicates(
-            points.index.name, keep="first"
-        )
-        assert points_parcels.shape[0] == points.shape[0]
+            points_.index.name = "points_index"
+        if type(points_) == gpd.GeoDataFrame:
+            points_["qk"] = points_.geometry.apply(
+                lambda p: quadkey.from_geo((p.y, p.x), level=zoom)
+            )
+        else:
+            points_["qk"] = points_.apply(
+                lambda row: str(
+                    quadkey.from_geo(
+                        (row[latitude_col], row[longitude_col]), level=zoom
+                    )
+                ),
+                axis=1,
+            )
+        points_crosswalk = points_.join(self.crosswalk_data, how="left", on="qk")
+        if drop_qk_col:
+            points_crosswalk.drop(["qk"], axis=1, inplace=True)
+        assert points_crosswalk.shape[0] == points.shape[0]
 
         # join boundaries aggregated data
         drop_xtra_cols = ["id", "name", "geometry"]
-        gdf_joined = points_parcels.copy()
-        for bnd, df_bnd in self.boundary_data.items():
-            gdf_joined = gdf_joined.merge(
+        points_crosswalk.reset_index(inplace=True)
+        df_joined = points_crosswalk.copy()
+        for bnd in self.bounds_features:
+            df_bnd = self.boundary_data[bnd]
+            df_joined = df_joined.merge(
                 df_bnd,
                 how="left",
                 left_on=f"{bnd}_id",
                 right_on=f"id_{bnd}",
             )
             for col in drop_xtra_cols:
-                try:
-                    gdf_joined.drop([f"{col}_{bnd}"], axis=1, inplace=True)
-                except KeyError:
-                    pass
-        assert gdf_joined.shape[0] == points.shape[0]
-        gdf_joined.set_index(points.index.name, inplace=True)
+                if f"{col}_{bnd}" not in self.bounds_features[bnd]:
+                    try:
+                        df_joined.drop([f"{col}_{bnd}"], axis=1, inplace=True)
+                    except KeyError:
+                        pass
+        df_joined.set_index(points_.index.name, inplace=True)
+        assert df_joined.shape[0] == points.shape[0]
 
         # remove extraneous columns
-        drop_cols = [c for c in self.parcel_data.columns if "geometry" not in c] + [
-            "index_right"
-        ]
-        gdf_joined.drop(drop_cols, axis=1, inplace=True)
+        drop_cols = [c for c in self.crosswalk_data.columns]
+        df_joined.drop(drop_cols, axis=1, inplace=True)
 
         # clean up data types
-        bool_cols = [c for c in gdf_joined.columns if "intersects" in c]
+        bool_cols = [c for c in df_joined.columns if "intersects" in c]
         for c in bool_cols:
-            gdf_joined[c] = gdf_joined[c].astype(float)
+            df_joined[c] = df_joined[c].astype(float)
 
-        return gdf_joined
+        return df_joined
--- tests/test_local_iggy_data_package.py
+++ tests/test_local_iggy_data_package.py
@@ -1,69 +1,104 @@
+import pandas as pd
 import geopandas as gpd
+from shapely import wkt
 
 from iggyenrich.iggy_data_package import LocalIggyDataPackage
 
 
 BASE_LOC = "sample_data"
-TEST_BOUNDS = ["isochrone_walk_10m", "cbg", "county"]
+IGGY_VERSION_ID = "20211110214810"
+CROSSWALK_PREFIX = "fl_pinellas_quadkeys"
+IGGY_PREFIX = "fl_pinellas_quadkeys"
+
+TEST_BOUNDS = ["qk_isochrone_walk_10m", "cbg", "county", "locality"]
 TEST_FEATURES = [
-    "area_sqkm_isochrone_walk_10m",
-    "population_isochrone_walk_10m",
-    "poi_count_per_capita_isochrone_walk_10m",
-    "poi_count_isochrone_walk_10m",
-    "poi_is_transportation_count_isochrone_walk_10m",
-    "poi_is_restaurant_count_isochrone_walk_10m",
-    "poi_is_social_and_community_services_count_isochrone_walk_10m",
-    "poi_is_religious_organization_count_per_capita_isochrone_walk_10m",
-    "park_intersecting_area_in_sqkm_isochrone_walk_10m",
-    "coast_intersecting_length_in_km_isochrone_walk_10m",
+    "area_sqkm_qk_isochrone_walk_10m",
+    "population_qk_isochrone_walk_10m",
+    "poi_count_per_capita_qk_isochrone_walk_10m",
+    "poi_count_qk_isochrone_walk_10m",
+    "poi_is_transportation_count_qk_isochrone_walk_10m",
+    "poi_is_restaurant_count_qk_isochrone_walk_10m",
+    "poi_is_social_and_community_services_count_qk_isochrone_walk_10m",
+    "poi_is_religious_organization_count_per_capita_qk_isochrone_walk_10m",
+    "park_intersecting_area_in_sqkm_qk_isochrone_walk_10m",
+    "coast_intersecting_length_in_km_qk_isochrone_walk_10m",
+    "perimeter_km_cbg",
     "coast_intersects_cbg",
     "acs_pop_employment_status_in_labor_force_civilian_unemployed_cbg",
     "acs_pct_households_with_no_internet_access_cbg",
     "acs_median_rent_cbg",
     "acs_median_year_structure_built_cbg",
+    "id_cbg",
 ]
 
 
 def test_load_package_basic():
     pkg = LocalIggyDataPackage(
         base_loc=BASE_LOC,
-        iggy_version_id="20211101222726",
-        parcels_prefix="fl_pinellas_parcels",
-        iggy_prefix="fl_pinellas_parcels",
+        iggy_version_id=IGGY_VERSION_ID,
+        crosswalk_prefix=CROSSWALK_PREFIX,
+        iggy_prefix=IGGY_PREFIX,
     )
     pkg.load(boundaries=TEST_BOUNDS)
     assert len(pkg.boundary_data) == len(TEST_BOUNDS)
     return pkg
 
 
-def test_load_package_features():
-    pkg = LocalIggyDataPackage(
-        base_loc=BASE_LOC,
-        iggy_version_id="20211101222726",
-        parcels_prefix="fl_pinellas_parcels",
-        iggy_prefix="fl_pinellas_parcels",
-    )
+def test_load_package_features(pkg: LocalIggyDataPackage):
     pkg.load(boundaries=["county"], features=TEST_FEATURES)
     assert (
-        pkg.boundary_data["cbg"].shape[1]
-        == len([f for f in TEST_FEATURES if f.endswith("_cbg")]) + 1
+        pkg.boundary_data["qk_isochrone_walk_10m"].shape[1]
+        == len([f for f in TEST_FEATURES if f.endswith("qk_isochrone_walk_10m")]) + 1
     )
     return pkg
 
 
-def test_enrich_features(pkg: LocalIggyDataPackage) -> gpd.GeoDataFrame:
-    test_points = (
-        pkg.parcel_data.sample(n=10).to_crs("EPSG:3857").centroid.to_crs("WGS84")
+def test_enrich_features_df(pkg: LocalIggyDataPackage) -> pd.DataFrame:
+    test_points = [
+        wkt.loads(pt)
+        for pt in pkg.crosswalk_data.quadkey_centroid_geometry.sample(n=25)
+    ]
+    test_df = pd.DataFrame(
+        {
+            "point_id": [2 ** x for x in range(len(test_points))],
+            "latitude": [pt.y for pt in test_points],
+            "longitude": [pt.x for pt in test_points],
+        },
     )
+    test_df.set_index("point_id", inplace=True)
+    enriched_points = pkg.enrich(test_df)
+    assert enriched_points.index.name == "point_id"
+    assert enriched_points.shape[0] == test_df.shape[0]
+    assert type(enriched_points) == pd.DataFrame
+    for feature_name in TEST_FEATURES:
+        assert feature_name in enriched_points.columns
+    return enriched_points
+
+
+def test_enrich_features_gdf(pkg: LocalIggyDataPackage) -> gpd.GeoDataFrame:
+    test_points = [
+        wkt.loads(pt)
+        for pt in pkg.crosswalk_data.quadkey_centroid_geometry.sample(n=25)
+    ]
+    points_geoms = gpd.GeoSeries(test_points, crs="WGS84")
     test_gdf = gpd.GeoDataFrame(
-        {"point_id": range(len(test_points)), "geometry": test_points},
-        geometry="geometry",
+        {
+            "point_id": [2 ** x for x in range(len(test_points))],
+        },
+        geometry=points_geoms,
     )
-    test_gdf.set_index("point_id", inplace=True)
     enriched_points = pkg.enrich(test_gdf)
+    assert enriched_points.shape[0] == test_gdf.shape[0]
+    assert type(enriched_points) == gpd.GeoDataFrame
+    for feature_name in TEST_FEATURES:
+        assert (
+            feature_name in enriched_points.columns
+        ), f"{feature_name} not found in enriched points columns"
     return enriched_points
 
 
 if __name__ == "__main__":
-    pkg = test_load_package_features()
-    enriched_points = test_enrich_features(pkg)
+    pkg = test_load_package_basic()
+    pkg = test_load_package_features(pkg)
+    enriched_points_df = test_enrich_features_df(pkg)
+    enriched_points_gdf = test_enrich_features_gdf(pkg)
